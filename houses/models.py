0d5d52d177adc8e956a485a967bf62eed0a9af46
peterlah <peterlah@naver.com>
2023-12-20 21:38:06 +0900

    5.5

--- houses/models.py
+++ houses/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
+
 class House(models.Model):
 
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField(verbose_name="Price", help_text="Positive Numbers Only")
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Numbers Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True, help_text="Are pets allowed in this house?")
+    pets_allowed = models.BooleanField(
+        default=True, help_text="Are pets allowed in this house?"
+    )
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
